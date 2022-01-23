dda76965b6977455a626827bd8c194e4736e1c46
Adil Hassan <adlhasn@gmail.com>
2022-01-23 15:31:07 +0000

    parameterized promotionIsApplied test in XPercentOffOnASkuUnitPricePromotionTest

--- src/test/java/com/adilhassan/maersk/promotionengine/XPercentOffOnASkuUnitPricePromotionTest.java
+++ src/test/java/com/adilhassan/maersk/promotionengine/XPercentOffOnASkuUnitPricePromotionTest.java
@@ -3,6 +3,8 @@ package com.adilhassan.maersk.promotionengine;
 import com.adilhassan.maersk.domain.Cart;
 import com.adilhassan.maersk.domain.SKU;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,17 +31,18 @@ class XPercentOffOnASkuUnitPricePromotionTest {
     assertFalse(promotion.isApplicable(cart));
   }
 
-  @Test
-  public void promotionIsApplied() {
+  @ParameterizedTest
+  @CsvSource(value = {"1:A:5", "2:A:10", "1:B:3", "2:C:4"}, delimiter = ':')
+  public void promotionIsApplied(int skuCount, SKU sku, double discount) {
     //Given
-    cart.addSku(SKU.A);
-    final Promotion promotion = new XPercentOffOnASkuUnitPricePromotion(10, SKU.A);
+    cart.addSkus(skuCount, sku);
+    final Promotion promotion = new XPercentOffOnASkuUnitPricePromotion(10, sku);
 
     //When
     cart.addPromotion(promotion);
     cart.applyPromotions();
 
     //Then
-    assertEquals(5, promotion.getDiscount());
+    assertEquals(discount, promotion.getDiscount());
   }
 }
