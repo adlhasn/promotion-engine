84445444e29abd1eb0c8160d55621523fc755706
Adil Hassan <adlhasn@gmail.com>
2022-01-23 19:42:40 +0000

    fixed failing tests

--- src/main/java/com/adilhassan/maersk/promotionengine/XPercentOffOnASkuUnitPricePromotion.java
+++ src/main/java/com/adilhassan/maersk/promotionengine/XPercentOffOnASkuUnitPricePromotion.java
@@ -12,6 +12,7 @@ public class XPercentOffOnASkuUnitPricePromotion implements Promotion {
   private double discount;
 
   public XPercentOffOnASkuUnitPricePromotion(final double percent, final SKU sku) {
+    validate(percent, sku);
     this.percent = percent;
     this.sku = sku;
   }
@@ -35,5 +36,18 @@ public class XPercentOffOnASkuUnitPricePromotion implements Promotion {
   @Override
   public List<SKU> getSkus() {
     return List.of(sku);
+  }
+
+  private void validate(final double percent, final SKU sku) {
+    final StringBuilder exceptionMessage = new StringBuilder();
+
+    if (percent <= 0) {
+      exceptionMessage.append("Percentage value must be greater than 0.");
+    }
+    if (sku == null) {
+      exceptionMessage.append("Sku must not be null.");
+    }
+    if (exceptionMessage.length() > 0)
+      throw new IllegalArgumentException(exceptionMessage.toString());
   }
 }
