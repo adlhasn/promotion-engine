9eff3c270ce6976cf035bc7516698fea8213120e
Adil Hassan <adlhasn@gmail.com>
2022-01-23 12:04:53 +0000

    added a bunch of tests for the cart

--- src/main/java/com/adilhassan/maersk/domain/SKU.java
+++ src/main/java/com/adilhassan/maersk/domain/SKU.java
@@ -1,4 +1,15 @@
 package com.adilhassan.maersk.domain;
 
 public enum SKU {
+  A(50), B(30), C(20), D(15);
+
+  private final double price;
+
+  SKU(final double price) {
+    this.price = price;
+  }
+
+  public double getPrice() {
+    return price;
+  }
 }
--- src/test/java/com/adilhassan/maersk/domain/CartTest.java
+++ src/test/java/com/adilhassan/maersk/domain/CartTest.java
@@ -1,7 +1,56 @@
 package com.adilhassan.maersk.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class CartTest {
 
+  @Test
+  public void canAddASingleSkuToCart() {
+
+  }
+
+  @Test
+  public void canAddMultipleSkusToCartAtOnce() {
+
+  }
+
+  @Test
+  public void canRemoveASkuFromTheCart() {
+
+  }
+
+  @Test
+  public void canAddAPromotionToCart() {
+
+  }
+
+  @Test
+  public void canAddMultiplePromotionsToCartAtOnce() {
+
+  }
+
+  @Test
+  public void canApplyPromotionsToCart() {
+
+  }
+
+  @Test
+  public void canGetTotalPriceOfTheCartWithoutPromotionalDiscount() {
+
+  }
+
+  @Test
+  public void canGetTotalPriceOfTheCartWithPromotionalDiscount() {
+
+  }
+
+  @Test
+  public void canGetNumberOfItemsOfParticularSkuInTheCart() {
+
+  }
+
+  @Test
+  public void canGetThePriceOfAGivenSkuInTheCart() {
+
+  }
 }
