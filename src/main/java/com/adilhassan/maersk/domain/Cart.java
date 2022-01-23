fbfc801b97dae823665a346bdd4d70b13b0d70f4
Adil Hassan <adlhasn@gmail.com>
2022-01-23 12:22:36 +0000

    added tests for adding and removing skus from the Cart

--- src/main/java/com/adilhassan/maersk/domain/Cart.java
+++ src/main/java/com/adilhassan/maersk/domain/Cart.java
@@ -1,4 +1,48 @@
 package com.adilhassan.maersk.domain;
 
+import com.adilhassan.maersk.promotionengine.PromotionEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Cart {
+  private final List<SKU> skus;
+  private final Map<SKU, Integer> skuCount;
+  private final Map<SKU, Double> skuPrice;
+  private final PromotionEngine promotionEngine;
+
+  public Cart() {
+    skus = new ArrayList<>();
+    skuCount = new ConcurrentHashMap<>();
+    skuPrice = new ConcurrentHashMap<>();
+    promotionEngine = new PromotionEngine();
+  }
+
+  public boolean addSku(final SKU sku) {
+    if (sku != null)
+      return skus.add(sku);
+
+    return false;
+  }
+
+  public boolean addSkus(int count, final SKU sku) {
+    if (count > 0 && sku != null) {
+      for (int i=0; i<count; i++) {
+        skus.add(sku);
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  public List<SKU> getSkus() {
+    return skus;
+  }
+
+  public void removeSku(SKU sku) {
+    skus.remove(sku);
+  }
 }
--- src/test/java/com/adilhassan/maersk/domain/CartTest.java
+++ src/test/java/com/adilhassan/maersk/domain/CartTest.java
@@ -1,22 +1,50 @@
 package com.adilhassan.maersk.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CartTest {
 
+  final Cart cart = new Cart();
+
   @Test
   public void canAddASingleSkuToCart() {
+    //Given
+    final SKU A = SKU.A;
 
+    //When
+    cart.addSku(A);
+
+    //Then
+    assertEquals(1, cart.getSkus().size());
   }
 
   @Test
   public void canAddMultipleSkusToCartAtOnce() {
+    //Given
+    final SKU A = SKU.A;
 
+    //When
+    cart.addSkus(3, A);
+
+    //Then
+    assertEquals(3, cart.getSkus().size());
   }
 
   @Test
   public void canRemoveASkuFromTheCart() {
+    //Given
+    final SKU A = SKU.A;
+    cart.addSku(A);
 
+    //When
+    cart.removeSku(A);
+
+    //Then
+    assertEquals(0, cart.getSkus().size());
   }
 
   @Test
