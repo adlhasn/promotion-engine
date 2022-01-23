16bbd18513df1d691a030ad4afc93350db483634
Adil Hassan <adlhasn@gmail.com>
2022-01-23 16:17:38 +0000

    added more test cases to PromotionEngine to apply multiple promotions on a cart

--- src/test/java/com/adilhassan/maersk/promotionengine/PromotionEngineTest.java
+++ src/test/java/com/adilhassan/maersk/promotionengine/PromotionEngineTest.java
@@ -39,7 +39,7 @@ class PromotionEngineTest {
   }
 
   @Test
-  public void canApplyPromotions() {
+  public void canApplyASinglePromotion() {
     //Given
     cart.addSkus(3, SKU.A);
     final Promotion promotion = new NSkusForAFixedPricePromotion(3, SKU.A, 130);
@@ -50,5 +50,79 @@ class PromotionEngineTest {
 
     //Then
     assertEquals(20, promotionEngine.getDiscount());
+    assertEquals(130, cart.getTotal());
+  }
+
+  @Test
+  public void noPromotionsAreApplied_ScenarioA() {
+    //Given
+    cart.addSku(SKU.A);
+    cart.addSku(SKU.B);
+    cart.addSku(SKU.C);
+
+    final Promotion nSkusForAFixedPricePromotion_A = new NSkusForAFixedPricePromotion(3, SKU.A, 130);
+    final Promotion nSkusForAFixedPricePromotion_B = new NSkusForAFixedPricePromotion(2, SKU.B, 45);
+    final Promotion twoSkusForAFixedPricePromotion_CD = new TwoSkusForAFixedPricePromotion(SKU.C, SKU.D, 30);
+
+    promotionEngine.addPromotions(List.of
+        (nSkusForAFixedPricePromotion_A,
+        nSkusForAFixedPricePromotion_B,
+        twoSkusForAFixedPricePromotion_CD));
+
+    //When
+    promotionEngine.applyPromotions();
+
+    //Then
+    assertEquals(0, promotionEngine.getDiscount());
+    assertEquals(100, cart.getTotal());
+  }
+
+  @Test
+  public void multiplePromotionsAreApplied_ScenarioB() {
+    //Given
+    cart.addSkus(5, SKU.A);
+    cart.addSkus(5, SKU.B);
+    cart.addSkus(1, SKU.C);
+
+    final Promotion nSkusForAFixedPricePromotion_A = new NSkusForAFixedPricePromotion(3, SKU.A, 130);
+    final Promotion nSkusForAFixedPricePromotion_B = new NSkusForAFixedPricePromotion(2, SKU.B, 45);
+    final Promotion twoSkusForAFixedPricePromotion_CD = new TwoSkusForAFixedPricePromotion(SKU.C, SKU.D, 30);
+
+    promotionEngine.addPromotions(List.of
+        (nSkusForAFixedPricePromotion_A,
+            nSkusForAFixedPricePromotion_B,
+            twoSkusForAFixedPricePromotion_CD));
+
+    //When
+    promotionEngine.applyPromotions();
+
+    //Then
+    assertEquals(50, promotionEngine.getDiscount());
+    assertEquals(370, cart.getTotal());
+  }
+
+  @Test
+  public void multiplePromotionsAreApplied_ScenarioC() {
+    //Given
+    cart.addSkus(3, SKU.A);
+    cart.addSkus(5, SKU.B);
+    cart.addSkus(1, SKU.C);
+    cart.addSkus(1, SKU.D);
+
+    final Promotion nSkusForAFixedPricePromotion_A = new NSkusForAFixedPricePromotion(3, SKU.A, 130);
+    final Promotion nSkusForAFixedPricePromotion_B = new NSkusForAFixedPricePromotion(2, SKU.B, 45);
+    final Promotion twoSkusForAFixedPricePromotion_CD = new TwoSkusForAFixedPricePromotion(SKU.C, SKU.D, 30);
+
+    promotionEngine.addPromotions(List.of
+        (nSkusForAFixedPricePromotion_A,
+            nSkusForAFixedPricePromotion_B,
+            twoSkusForAFixedPricePromotion_CD));
+
+    //When
+    promotionEngine.applyPromotions();
+
+    //Then
+    assertEquals(55, promotionEngine.getDiscount());
+    assertEquals(280, cart.getTotal());
   }
 }
